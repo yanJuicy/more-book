e116e2cb2f903b12ccf7f06c00515d1155bf56dd
yanJuicy <hanjub3@gmail.com>
2024-10-09 21:51:33 +0900

    [chore] 프로젝트 생성

---
+++ src/main/java/app/morebook/MorebookApplication.java
@@ -0,0 +1,13 @@
+package app.morebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/morebook/MorebookApplicationTests.java
@@ -0,0 +1,13 @@
+package app.morebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
